b028ab71e099213643a5d649854cf5df041f40fd
BaptisteAuscher <baptiste.auscher@gmail.com>
2020-10-19 16:26:45 +0200

    attempted to correct a bug, not finished

--- server/server.js
+++ server/server.js
@@ -5,14 +5,24 @@ const ytdl = require('ytdl-core')
 
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('Not supposed to be there')
+})
+
 app.get('/download', async (req, res) => {
+
     let URL = req.query.URL
+    if (!URL) {
+        res.send('No url there')
+    }
 
     if (ytdl.validateURL(URL)) {
         let info = await ytdl.getInfo(URL)
         res.header('Content-Disposition', `attachment;filename="${info.videoDetails.title}.mp4"`)
         ytdl(URL, {quality: 'highest'}).pipe(res);
-    } 
+    } else {
+        res.send('Not valid url')
+    }
 
 })
 
